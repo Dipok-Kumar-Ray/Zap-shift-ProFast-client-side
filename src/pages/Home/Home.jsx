6c6b34d2418979a9c0f12519d20ed084b2d6fb4d
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-26 00:38:10 +0600

    Create benefits section implement

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Services from '../services/Services';
 import ClientLogosMarquee from '../clientLogosMarquee/ClientLogosMarquee';
+import Benefits from '../benefits/Benefits';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            <Banner/>
            <Services/>
            <ClientLogosMarquee/>
+           <Benefits/>
         </div>
     );
 };
