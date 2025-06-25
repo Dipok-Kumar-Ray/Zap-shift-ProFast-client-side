b30fd1124843850a4d74351ba8865cec16ff95b4
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-25 17:41:09 +0600

    added services six cards implementations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Services from '../services/Services';
+import ClientLogosMarquee from '../clientLogosMarquee/ClientLogosMarquee';
 
 const Home = () => {
     return (
         <div>
            <Banner/>
+           <Services/>
+           {/* <ClientLogosMarquee/> */}
         </div>
     );
 };
