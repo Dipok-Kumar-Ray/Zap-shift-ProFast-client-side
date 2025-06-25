20af8505b67d01c83c46233f5db39e9a6fd464c7
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-25 17:43:14 +0600

    Added branding logos and marquee implementations

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div>
            <Banner/>
            <Services/>
-           {/* <ClientLogosMarquee/> */}
+           <ClientLogosMarquee/>
         </div>
     );
 };
