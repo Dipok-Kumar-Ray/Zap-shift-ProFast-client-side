7fb99cdcff27ce60fdd2efafa50b19a77807e127
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-27 02:21:56 +0600

    added AuthLayout

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login";
+
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +14,17 @@ export const router = createBrowserRouter([
         index: true,
         Component: Home
     }
+    
    ]
   },
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        Component: Login
+      }
+    ]
+  }
 ]);
