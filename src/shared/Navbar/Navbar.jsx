f0d4e9e1be4d2b6891b24479ac7d84772f80214d
Dipok Kumar Ray <dipok7316@gmail.com>
2025-06-30 23:59:31 +0600

    added dashboard Layoaut

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -26,6 +26,9 @@ const Navbar = () => {
       <li>
         <NavLink to="/coverage">Coverage</NavLink>
       </li>
+      {
+        
+      }
       <li>
         <NavLink to="/about">About Us</NavLink>
       </li>
